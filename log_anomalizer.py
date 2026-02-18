a0ec215e11a809ccb2f8ec1d07b713c68c361801
Abhinav Babukuttan <abhinavbabu33@gmail.com>
2026-02-18 17:53:17 +0530

    Update log_anomalizer.py

--- log_anomalizer.py
+++ log_anomalizer.py
@@ -1,20 +1,20 @@
 freq = {}
-file_name = input("Enter filename: ")
+#file_name = input("Enter filename: ")
 tot = 0
+with open("sample.log","w") as f1:
+  f1.write("Practice a code every day")
+  f1.write("Hello world Hello")
 try:
-    with open(file_name,"r") as f:
-        num_lines = len(f.readlines())
+  with open("sample.log","r") as f:
+    for line in f:
+      for word in line:
+        freq[word] = freq.get(word,0) + 1
+        tot+=1
+    print(freq)
+    for word in freq:
+      if freq[word] < 0.01 * tot:
         for line in f:
-            for word in line:
-                freq[word] = freq.get(word, 0) + 1
-                tot += 1
-        
-        for word in freq: 
-            if freq[word] < 0.01 * tot:
-                for line in f:
-                    if word in line:
-                        print(line)
-
+          if word in line:
+            print(line)
 except FileNotFoundError:
-    print("File not found")
- 
+  print("File not found")
